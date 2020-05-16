d957954edc09a0a276457c04ee1b810dcfe5f41c
Gilbert Acosta <acostagilberto95@gmail.com>
2020-05-15 20:54:34 -0400

    Query Selector all seguir

---
+++ universidad/js/app.js
@@ -0,0 +1,81 @@
+// let elemento;
+
+// elemento = document;
+// elemento = document.all;
+// elemento = document.all[10];
+// elemento = document.head;
+// elemento = document.body;
+// elemento = document.domain;
+// elemento = document.URL;
+// elemento = document.characterSet;
+// elemento = document.forms;
+// elemento = document.forms[0];
+// elemento = document.forms[0].id;
+// elemento = document.forms[0].className;
+// elemento = document.forms[0].classList;
+// elemento = document.forms[0].classList[0];
+// elemento = document.images;
+// elemento = document.images[2];
+// elemento = document.images[2].getAttribute("src");
+// elemento = document.scripts;
+// elemento = document.images;
+
+// let imagenes = document.images;
+
+// let imagenesArr = Array.from(imagenes);
+
+// imagenesArr.forEach((imagen) => console.log(imagen));
+
+// // console.log(elemento);
+// console.log(imagenesArr);
+// getElementById
+
+// let encabezado;
+
+// encabezado = document.getElementById("encabezado");
+
+// encabezado.style.background = "#333";
+// encabezado.style.color = "#fff";
+// encabezado.style.padding = "20px";
+// encabezado.textContent = "Los mejores Cursos";
+
+// console.log(encabezado);
+
+// Query Selector
+
+// const encabezado = document.querySelector(".card img");
+
+// Aplicar CSS
+// encabezado.style.background = "#333";
+// encabezado.style.color = "#fff";
+// encabezado.style.padding = "20px";
+// encabezado.textContent = "Los mejores cursos";
+
+// console.log(encabezado);
+
+// let enlace;
+
+// enlace = document.querySelector("#principal a:first-child");
+
+// enlace = document.querySelector("#principal a:nth-child(3)");
+
+// enlace = document.querySelector("#principal a:last-child");
+
+// console.log(enlace);
+// getElementByClassName
+
+// let enlaces = document.getElementsByClassName("enlace")[3];
+
+// enlaces.style.background = "#333";
+
+// console.log(enlaces);
+
+//getElementsByTagName
+// let links = document.getElementsByTagName("a");
+
+// let enlaces = Array.from(links);
+
+// enlaces.forEach((enlace) => {
+//   console.log(enlace.textContent);
+// });
+// console.log(links);
